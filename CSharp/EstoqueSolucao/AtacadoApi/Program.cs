aef05cb09e3fcb3028203dbb48c7fe7036d74422
InoueAkiraa <akiraij@hotmail.com>
2022-11-18 14:52:55 -0400

    Encontro_47
    Realizada criação da pasta Documents que vai armazenar o arquivo .xml.

--- CSharp/EstoqueSolucao/AtacadoApi/Program.cs
+++ CSharp/EstoqueSolucao/AtacadoApi/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddSwaggerGen(opcoes =>
     opcoes.SwaggerDoc("v1", new OpenApiInfo()
     {
         Version = "v1",
-        Title = "Atacado API",
+        Title = "ToDo API",
         Description = "An ASP.NET Core Web API for managing ToDo items",
         TermsOfService = new Uri("https://example.com/terms"),
         Contact = new OpenApiContact
@@ -28,8 +28,8 @@ builder.Services.AddSwaggerGen(opcoes =>
             Url = new Uri("https://example.com/license")
         }
     });
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    opcoes.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+    var filePath = Path.Combine(builder.Environment.ContentRootPath, @"Documents\AtacadoApi.xml");
+    opcoes.IncludeXmlComments(filePath);
 });
 var app = builder.Build();
 
